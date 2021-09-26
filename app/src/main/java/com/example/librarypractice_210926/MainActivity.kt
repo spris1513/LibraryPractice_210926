0970caba4cf9612f998e63b0399f52bd7d501143
이진호 <spris1513@gmail.com>
2021-09-26 15:08:51 +0900

    전화걸기(call)활용 > 권한없어서 앱죽음

--- app/src/main/java/com/example/librarypractice_210926/MainActivity.kt
+++ app/src/main/java/com/example/librarypractice_210926/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.librarypractice_210926
 
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
@@ -9,6 +10,18 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        callBtn.setOnClickListener {
+
+//            Intent(4) 바로 전화 연결 > CALL
+
+            val myUri = Uri.parse("010-5555-6666")
+            val myIntent = Intent(Intent.ACTION_CALL,myUri)
+            startActivity(myIntent)
+
+
+        }
+
 
         profileImg.setOnClickListener {
 
