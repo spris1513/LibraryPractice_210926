1681fe890f61cb5f3e40b15db77ecffddd288e28
이진호 <spris1513@gmail.com>
2021-09-26 14:52:51 +0900

    뭔지모르겠고, 사진크게보는화면까진 추가함

--- app/src/main/java/com/example/librarypractice_210926/MainActivity.kt
+++ app/src/main/java/com/example/librarypractice_210926/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.example.librarypractice_210926
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        profileImg.setOnClickListener {
+
+//            다른 화면으로 이동 > 사진 크게 보기
+
+            val myIntent = Intent(this,ViewPhotoActivity::class.java)
+            startActivity(myIntent)
+
+
+        }
     }
 }
