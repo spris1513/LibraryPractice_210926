a8a6cdcd5979d3fcedab634cdbbd33d570e187bc
이진호 <spris1513@gmail.com>
2021-09-26 15:44:56 +0900

    실제 권한 물어보기

--- app/src/main/java/com/example/librarypractice_210926/MainActivity.kt
+++ app/src/main/java/com/example/librarypractice_210926/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.example.librarypractice_210926
 
+import android.Manifest
 import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import com.gun0912.tedpermission.PermissionListener
+import com.gun0912.tedpermission.normal.TedPermission
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -30,10 +32,16 @@ class MainActivity : AppCompatActivity() {
 
                 override fun onPermissionDenied(deniedPermissions: MutableList<String>?) {
 //                    권한이 거절 되었을때 > 보통 토스트
-                    Toast.makeText(this, "권한이 거절되어 전화 연결이 불가합니다.", Toast.LENGTH_SHORT).show()
+                    Toast.makeText(this@MainActivity, "권한이 거절되어 전화 연결이 불가합니다.", Toast.LENGTH_SHORT).show()
                 }
 
             }
+
+//            pl에 적혀있는 가이드를 들고 > 실제 권한 확인
+            TedPermission.create()
+                .setPermissionListener(pl)
+                .setPermissions(Manifest.permission.CALL_PHONE)
+                .check()
 
 
 
